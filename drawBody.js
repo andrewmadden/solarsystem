f35f8d3e0b7b95c9f920ff2c2ba23625f92a2ad0
Andrew Madden <andrewmaddendev@gmail.com>
2019-01-29 20:26:49 +1100

    created drawBody js

---
+++ drawBody.js
@@ -0,0 +1,8 @@
+class drawBodies {
+    constructor(ctx, trailLength, radius) {
+        this.ctx = ctx;
+        this.trailLength = trailLength;
+        this.radius = radius;
+        this.positions = [];
+    }
+}
