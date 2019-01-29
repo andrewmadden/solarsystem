c122a7838fbf8df1c99655e9f8b310220a6473b7
Andrew Madden <andrewmaddendev@gmail.com>
2019-01-29 17:56:02 +1100

    nBodyProblem class

--- physics.js
+++ physics.js
@@ -10,7 +10,7 @@ const bodies = [
 // time: year
 
 // gravitational constant
-const g = 39.5
+// const g = 39.5
 
 class nBodyProblem {
     constructor(params) {
@@ -21,56 +21,56 @@ class nBodyProblem {
         this.bodies = params.bodies;
     }
 
-
-}
-
-// update the positions of each body for a set time interval, dt
-const updatePositionVectors = (bodies, dt) => {
-    bodies.forEach(body => {
-        body.x += body.vx * dt;
-        body.y += body.vy * dt;
-        body.z += body.vz * dt;
-    });
-}
-
-// update the velocities of each body for a set time interval, dt
-const updateVelocityVectors = (bodies, dt) => {
-    bodies.forEach(body => {
-        body.vx += body.ax * dt;
-        body.vy += body.ay * dt;
-        body.vz += body.az * dt;
-    });
-}
-
-// update the acceleraties of each body taking into account all bodies in system
-const updateAccelerationVectors = (bodies, g, softeningConstant) => {
-    // creates a copy of the bodies array.
-    // original array will be altered, copied one will not.
-    const bodiesCopy = JSON.parse(JSON.stringify(bodies));
-
-    bodies.forEach(body => {
-        body.ax = 0;
-        body.ay = 0;
-        body.az = 0;
-
-        bodiesCopy.forEach(bodyCopy => {
-            const dx = bodyCopy.x - body.x;
-            const dy = bodyCopy.y - body.y;
-            const dz = bodyCopy.z - body.z;
-
-            // squared distance between to bodies
-            const distSq = dx*dx + dy*dy + dz*dz;
-
-            // if the squared distance is zero, assume the body is itself and ignore
-            if (distSq != 0) {
-                // gravitational force exerted on each body
-                const f = (g * bodyCopy.mass) / (distSq * Math.sqrt(distSq + softeningConstant));
-    
-                body.ax += dx * f;
-                body.ay += dy * f;
-                body.az += dz * f;
-            }
-            
+    // update the positions of each body for a set time interval, dt
+    updatePositionVectors() {
+        this.bodies.forEach(body => {
+            body.x += body.vx * this.dt;
+            body.y += body.vy * this.dt;
+            body.z += body.vz * this.dt;
         });
-    });
+    }
+    
+    // update the velocities of each body for a set time interval, dt
+    updateVelocityVectors() {
+        this.bodies.forEach(body => {
+            body.vx += body.ax * this.dt;
+            body.vy += body.ay * this.dt;
+            body.vz += body.az * this.dt;
+        });
+    }
+
+    // update the acceleraties of each body taking into account all bodies in system
+    updateAccelerationVectors() {
+        // creates a copy of the bodies array.
+        // original array will be altered, copied one will not.
+        const bodiesCopy = JSON.parse(JSON.stringify(this.bodies));
+    
+        this.bodies.forEach(body => {
+            body.ax = 0;
+            body.ay = 0;
+            body.az = 0;
+    
+            bodiesCopy.forEach(bodyCopy => {
+                const dx = bodyCopy.x - body.x;
+                const dy = bodyCopy.y - body.y;
+                const dz = bodyCopy.z - body.z;
+    
+                // squared distance between to bodies
+                const distSq = dx*dx + dy*dy + dz*dz;
+    
+                // if the squared distance is zero, assume the body is itself and ignore
+                if (distSq != 0) {
+                    // gravitational force exerted on each body
+                    const f = (this.g * bodyCopy.mass) / (distSq * Math.sqrt(distSq + this.softeningConstant));
+        
+                    body.ax += dx * f;
+                    body.ay += dy * f;
+                    body.az += dz * f;
+                }
+                
+            });
+        });
+    }
 }
+
+
